15312f8af5cc7246ccdfacfb237c34ca20191aa4
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-11-27 09:41:13 -0300

    test : testing CI in container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
+// parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23207052#overview
+
 export default () => {
   return (
     <div>
-      <h1>Hi Container!</h1>
+      <h1>Hi Container!!</h1>
       <hr />
       <MarketingApp />
     </div>
