ea36b016f043141908bd4745a81e6871251b7d85
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-11-29 08:37:10 -0300

    feat : provided classes names prefixs to avoid colisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
-// parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23207052#overview
+// parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23241664#notes
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'containerApp',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-          <Header />
-          <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+            <Header />
+            <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
