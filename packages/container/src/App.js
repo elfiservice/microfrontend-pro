4901d80fecd24ea8f5730256bd662530ec21758e
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-11-29 07:50:45 -0300

    feat : provided a general header component to container App

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 // parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23207052#overview
 
 export default () => {
   return (
-    <div>
-      <h1>Hi Container!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+          <Header />
+          <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
