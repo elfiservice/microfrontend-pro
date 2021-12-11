929df1ec0722ec1cc9eacc7ea34cfe00a6a6b411
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-12-11 09:54:16 -0300

    fix : to navigation works in isolation microfront App

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // creado esse history para que cada Microfront fique independente na navegação
 // https://www.udemy.com/course/microfrontend-course/learn/lecture/23241682#notes
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
 const mount = (el, config) => {
-  const { onNavigate } = config;
-  const history = createMemoryHistory();
+  const { onNavigate, defaultHistory } = config;
+  const history = defaultHistory || createMemoryHistory();
 
   const noticeContainerApp = {
     onParentNavigate(location) {
@@ -36,7 +36,11 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {});
+    const config = {
+      defaultHistory: createBrowserHistory()
+    };
+
+    mount(devRoot, config);
   }
 }
 
