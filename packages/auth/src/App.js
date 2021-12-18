1690ba13b7d94d453bfc74c04594142db4ea588c
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-12-18 08:22:47 -0300

    feat : started the Auth App

---
+++ packages/auth/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+// import Landing from './components/Landing';
+// import Pricing from './components/Pricing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'authApp',
+});
+
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// creado esse history para que cada Microfront fique independente na navegação
+// https://www.udemy.com/course/microfrontend-course/learn/lecture/23241682#notes
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el, config) => {
+  const { onNavigate, defaultHistory } = config;
+  const history = defaultHistory || createMemoryHistory();
+
+  const noticeContainerApp = {
+    onParentNavigate(location) {
+      const { pathname: nextPathname } = location;
+      const { pathname } = history.location;
+      
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return noticeContainerApp;
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    const config = {
+      defaultHistory: createBrowserHistory()
+    };
+
+    mount(devRoot, config);
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
