a676ae814f4ae07978469d474c1373aed9fa94ee
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2021-12-26 10:51:16 -0300

    feat : added lazy loading sub Apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 
 //parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23275292
 //https://github.com/elfiservice/microfrontend-pro/pull/2
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'containerApp',
@@ -21,10 +22,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
             <Header />
-            <Switch>
-              <Route path="/auth" component={AuthApp} />
-              <Route path="/" component={MarketingApp} />
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route path="/auth" component={AuthLazy} />
+                <Route path="/" component={MarketingLazy} />
+              </Switch>
+            </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
