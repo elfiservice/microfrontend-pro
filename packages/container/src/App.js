e260d7d6be4e7b364bf60aa59fc4bc7206d88c46
Antonio Armando O de Araujo Junior <elfiservice@hotmail.com>
2022-01-09 09:37:21 -0300

    feat : communicating Authentication State

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
@@ -7,8 +7,6 @@ import {
 import Header from './components/Header';
 import Progress from './components/Progress'
 
-//parei aqui https://www.udemy.com/course/microfrontend-course/learn/lecture/23275292
-//https://github.com/elfiservice/microfrontend-pro/pull/2
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
@@ -18,14 +16,18 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+  const [ isSignedIn, setIsSignedIn ] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-            <Header />
+            <Header isSignedIn={isSignedIn} />
             <Suspense fallback={<Progress />}>
               <Switch>
-                <Route path="/auth" component={AuthLazy} />
+                <Route path="/auth">
+                  <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                </Route>
                 <Route path="/" component={MarketingLazy} />
               </Switch>
             </Suspense>
